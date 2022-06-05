d7da73ea0f1c0a4ba77de3c5895e608ef642ae3e
Amirhossein Sarajan <amirhossein.sarajan@seb.se>
2022-06-05 15:25:33 +0800

    fix the multiple product sales test

--- TopSalesTest/TopSalesTest.cs
+++ TopSalesTest/TopSalesTest.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using Xunit;
 
@@ -30,7 +32,7 @@ public class TopSalesTest
                 Name = " Product3",
             }
         };
-        GTINs = new string[] { "G#1111", "G#2222","G#3333" };
+        GTINs = new string[] { "G#1111", "G#2222", "G#3333" };
         salesService = new SalesService();
     }
 
@@ -46,8 +48,8 @@ public class TopSalesTest
     [Fact]
     public void Returns_the_only_top_sales_of_one_product()
     {
-        var orderLine = CreateOrderLine(0, 10);
-        var orderLine2 = CreateOrderLine(0, 15);
+        var orderLine = CreateTestOrderLine(0, 10, products, GTINs);
+        var orderLine2 = CreateTestOrderLine(0, 15, products, GTINs);
 
         var orders = new List<Order>() {
             new Order(){
@@ -73,54 +75,82 @@ public class TopSalesTest
     [Fact]
     public void Returns_topsales_of_multiple_products()
     {
-        var product1OrderLine = CreateOrderLine(0, 10);        
-        var product2OrderLine = CreateOrderLine(1, 15);
-        var product3OrderLine = CreateOrderLine(2, 5);
+        var testTuple = CreateTestProducts(10);
 
-        var orders = new List<Order>() {
-            new Order(){
-                Status = "IN_PROGRESS",
-                Lines = new List<OrderLine>(){
-                    product1OrderLine ,
-                    product2OrderLine
-                }
-            },
-            new Order(){
-                Status = "IN_PROGRESS",
-                Lines = new List<OrderLine>(){
-                    product3OrderLine
-                }
-            }
-        };
+        var testProducts = testTuple.Item1;
+        var testGtins = testTuple.Item2;
 
-        var topsales = salesService.GetTopSales(orders, products);
+        var orders = CreateTestOrders(10, testProducts, testGtins);
 
-        Assert.Collection(topsales,
-            firstTopSale =>
+        var topsales = salesService.GetTopSales(orders, testProducts).Take(5);
+
+        var expectedSale = Enumerable.Range(1, 10)
+            .Reverse()
+            .Select(index => new Sale()
             {
-                Assert.Equal(product2OrderLine.Quantity, firstTopSale.SoldQuantity);
-                Assert.Equal(products[1].Name, firstTopSale.ProductName);
-            },
-            secondTopSale =>
-            {
-                Assert.Equal(product1OrderLine.Quantity, secondTopSale.SoldQuantity);
-                Assert.Equal(products[0].Name, secondTopSale.ProductName);
-            },
-            thirdTopSales =>
-            {
-                Assert.Equal(product3OrderLine.Quantity, thirdTopSales.SoldQuantity);
-                Assert.Equal(products[2].Name, thirdTopSales.ProductName);
-            });
+                ProductName = testProducts[index - 1].Name,
+                GTIN = testGtins[index - 1],
+                SoldQuantity = index * 10
+            }).ToList();
+
+        Assert.Equal(expectedSale, topsales, new TopSaleComparer());
     }
 
-    private OrderLine CreateOrderLine(int productIndex, int quantity)
+    class TopSaleComparer : IEqualityComparer<Sale>
+    {
+        public bool Equals(Sale? x, Sale? y)
+        {
+            return x.GTIN == y.GTIN && x.ProductName == y.ProductName && x.SoldQuantity == y.SoldQuantity;
+        }
+
+        public int GetHashCode([DisallowNull] Sale obj)
+        {
+            return obj.GetHashCode();
+        }
+    }
+
+    private static OrderLine CreateTestOrderLine(
+        int productIndex, int quantity, IList<Product> products, string[] gtins)
     {
         return new OrderLine()
         {
             MerchantProductNo = products[productIndex].MerchantProductNo,
-            GTIN = GTINs[productIndex],
+            GTIN = gtins[productIndex],
             Quantity = quantity,
         };
     }
-    
+
+    private Tuple<IList<Product>, string[]> CreateTestProducts(int noProducts)
+    {
+        return new Tuple<IList<Product>, string[]>(
+            Enumerable.Range(1, noProducts)
+            .Select(index =>
+            {
+                return new Product()
+                {
+                    MerchantProductNo = $"#{index}",
+                    Name = $"Product{index}"
+                };
+            }).ToList(),
+            Enumerable.Range(1, noProducts)
+            .Select(index => $"G#{index}").ToArray());
+    }
+
+    private List<Order> CreateTestOrders(int noOrders, IList<Product> products, string[] gtins)
+    {
+        List<Order> orders = new List<Order>();
+        for (var orderIndex = 0; orderIndex < noOrders; orderIndex++)
+        {
+            var orderLines = Enumerable.Range(0, products.Count())
+                .Select(productIndex =>                
+                    CreateTestOrderLine(productIndex, productIndex + 1, products, gtins)
+                ).ToList();
+
+            orders.Add(new Order()
+            {
+                Lines = orderLines
+            });
+        }
+        return orders;
+    }
 }
